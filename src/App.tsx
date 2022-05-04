80454c885644e0dc257107cdbb72ba61db0ace1e
Artem <dino.olimp@mail.ru>
2022-05-04 15:49:31 +0400

    fix: fix remarks

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,7 @@
 import React from 'react';
-import Form from './components/form/Form';
-import * as yup from 'yup';
-
-const firstSchema = yup
-  .object()
-  .shape({
-    age: yup.number(),
-    name: yup.string().required(),
-  })
-  .required();
-
-const firstInitVal = {
-  name: '',
-  age: 0,
-};
-
-const firstFields = [{ name: 'name', inputClass: 'qwe' }, { name: 'age' }];
-
-type schema = {
-  name: string;
-  age: number;
-};
 
 function App() {
-  function onSubmit(values: schema) {
-    console.log(values);
-  }
-
-  return (
-    <div className='App'>
-      <Form
-        schema={firstSchema}
-        initialValues={firstInitVal}
-        fields={firstFields}
-        // requestData={{ url: 'url', methood: 'post' }}
-        onSubmit={onSubmit}
-        formId='formButton'
-      />
-      <button type='submit' form='formButton'>
-        Submit
-      </button>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
