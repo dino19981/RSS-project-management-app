de4d3916852ce588208dc4b0702b3e7b69ede40c
excluz1v <bmd4work@gmail.com>
2022-05-08 21:22:31 +0600

    refactor: delete copy component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Boards from './components/Boards/Boards';
 import Board from './components/Boards/Board/Board';
 import Column from './components/Columns/Column';
-import Columns from './components/Columns/Columns';
 
 function App() {
   return (
@@ -13,7 +12,7 @@ function App() {
           <Routes>
             <Route path="/boards" element={<Boards />} />
             <Route path="/boards/:boardId" element={<Board />} />
-            <Route path="/boards/:boardId/columns" element={<Columns />} />
+            <Route path="/boards/:boardId/columns" element={<Board />} />
             <Route path="/boards/:boardId/columns/:columnId" element={<Column />} />
           </Routes>
         </div>
