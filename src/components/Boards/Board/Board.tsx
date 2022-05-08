304dde332076fb3d848c0df4512990ec0f371645
excluz1v <bmd4work@gmail.com>
2022-05-08 18:30:53 +0600

    feat: create BOard component

--- src/components/Boards/Board/Board.tsx
+++ src/components/Boards/Board/Board.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { TColumn } from '../../../models/column';
+import Column from '../../Column/Column';
 
 function Board() {
-  return <div className="board">Board</div>;
+  useEffect(() => {
+    //TODO Загрузка  доски /board:id
+  }, []);
+
+  const columns: TColumn[] | undefined = [];
+
+  return (
+    <div className="board">
+      <div className="board_menu">
+        <button type="button">create Column</button>
+        <button type="button">delete board</button>
+      </div>
+
+      <div className="columns_wrapper">
+        {columns
+          ? columns.map((col) => {
+              return <Column key={col.id} column={col} />;
+            })
+          : null}
+      </div>
+    </div>
+  );
 }
 
 export default Board;
