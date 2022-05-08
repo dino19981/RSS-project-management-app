be13de99ce6ecd7ed568a70013f28cec7df0fc80
excluz1v <bmd4work@gmail.com>
2022-05-08 17:50:37 +0600

    feat: add task count to board component

--- src/components/Boards/Board.tsx/Board.tsx
+++ src/components/Boards/Board.tsx/Board.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { TBoard } from '../../../models/board';
+import { TColumn } from '../../../models/column';
 
 type TProps = {
   board: TBoard;
 };
 
+function calculateTask(columns: TColumn[] | undefined) {
+  if (columns === undefined) return null;
+  let tasks = 0;
+  columns.map((col) => {
+    if (col.tasks) {
+      tasks += col.tasks.length;
+    }
+  });
+  return tasks;
+}
+
 function Board(props: TProps) {
   const { id, title, columns } = props.board;
-  return <div>Board</div>;
+  const taskCount = calculateTask(columns);
+
+  return (
+    <Link to={`/board/${id}`}>
+      <div className="board">
+        <div className="board_title">{title}</div>
+        {taskCount ? <div className="board_task-count">Total tasks: {taskCount}</div> : null}
+      </div>
+    </Link>
+  );
 }
 
 export default Board;
