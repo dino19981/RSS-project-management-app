88d6c1f212b1901678244e09168ec6c283e3462d
Roman Nasyrov <romnasi@gmail.com>
2022-05-10 02:22:16 +0300

    fix: fix button text

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
             <svg className="btn-new-board__icon" width="24" height="24">
               <use xlinkHref="#create-icon" />
             </svg>
-            <span className="btn-new-board__text">Create new board</span>
+            <span className="btn-new-board__text">Создать новую доску</span>
           </button>
         )}
 
