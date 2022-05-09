1ab7e66747160ccef491744fb53be24c3bb2585f
excluz1v <bmd4work@gmail.com>
2022-05-09 23:53:20 +0600

    feat: add classname to link

--- src/components/Boards/BoardPreview/BoardPreview.tsx
+++ src/components/Boards/BoardPreview/BoardPreview.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { TBoard } from '../../../models/board';
 import { TColumn } from '../../../models/column';
+import Button from '../../button/Button';
+import Modal from '../../modal/Modal';
 
 function calculateTask(columns: TColumn[] | undefined) {
   if (columns === undefined) return null;
@@ -17,13 +19,39 @@ function calculateTask(columns: TColumn[] | undefined) {
 function BoardPreview({ id, title, columns }: TBoard) {
   const taskCount = calculateTask(columns);
 
+  const [isModalActive, setModal] = useState(false);
+
+  function closeModal() {
+    setModal(false);
+  }
+
+  function openModal() {
+    setModal(true);
+  }
+  async function deleteBoard() {
+    console.log('delete board');
+  }
+
   return (
-    <Link to={`/boards/${id}`}>
-      <div className="board_preview">
+    <div className="board_preview">
+      <Link to={`/boards/${id}`} className="board_preview__link">
         <div className="board_preview_title">{title}</div>
         {taskCount && <div className="board_preview__task-count">Total tasks: {taskCount}</div>}
+      </Link>
+      <div className="board_preview_footer">
+        <Button handler={openModal} text="delete" />
+        {isModalActive && (
+          <Modal
+            formId="modalForm"
+            handleCloseModal={closeModal}
+            submitBtnName="OK"
+            handler={deleteBoard}
+          >
+            <div>Are you sure ?</div>
+          </Modal>
+        )}
       </div>
-    </Link>
+    </div>
   );
 }
 
