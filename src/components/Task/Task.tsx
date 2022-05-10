a2791751bd258734c7dd0561dc8c51de2738a5cf
excluz1v <bmd4work@gmail.com>
2022-05-10 15:50:13 +0600

    feat: Add modal window to Task component

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as yup from 'yup';
+import Form from '../form/Form';
+import Modal from '../modal/Modal';
+
+const schema = yup
+  .object()
+  .shape({
+    title: yup.string().trim().required(),
+    description: yup.string().trim().required(),
+  })
+  .required();
+
+const fields = [
+  //TODO разобраться с полями
+  { name: 'title', errorMessage: 'Title is required', placeholder: 'task title' },
+  { name: 'description', errorMessage: 'description is required', placeholder: 'description' },
+];
 
 function Task() {
-  return <div className="task">Task</div>;
+  const [isModalActive, setModal] = useState(true);
+  const initialValues = {
+    title: '',
+    description: '',
+  };
+  useEffect(() => {
+    // TODO  FETCH GET /boards/:boardId/columns/:columnId/tasks/:taskId
+    // initialValuess = response
+  }, []);
+
+  const formOptions = {
+    schema,
+    initialValues,
+    fields,
+  };
+
+  function closeModal() {
+    console.log('close modal');
+    setModal(false);
+  }
+
+  function saveTask() {
+    // TODO  FETCH PUT /boards/:boardId/columns/:columnId/tasks/:taskId
+    console.log('save task');
+  }
+
+  return (
+    <div className="task">
+      <Modal formId="modalForm-task" handleCloseModal={closeModal} submitBtnName="save task">
+        <Form
+          formId="modalForm-task"
+          {...formOptions}
+          onSubmit={saveTask}
+          buttonOptions={{ text: '123' }}
+        />
+      </Modal>
+    </div>
+  );
 }
 
 export default Task;
