523c5dc38a9668cef4aa79ce5cee726db0a431dd
excluz1v <bmd4work@gmail.com>
2022-05-10 14:45:01 +0600

    feat: add link to /taskId in Taskpreview component

--- src/components/Task/TaskPreview.tsx
+++ src/components/Task/TaskPreview.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { TTask } from '../../models/task';
 
-function TaskPreview() {
-  return <div>TaskPreview</div>;
+function TaskPreview({ id, title, description, order, userId, done }: TTask) {
+  const { pathname } = useLocation();
+  const urlToTask = pathname + `/${id}`;
+  return (
+    <div className="task-preview">
+      <Link to={urlToTask} className="task_link">
+        TaskPreview
+      </Link>
+    </div>
+  );
 }
 
 export default TaskPreview;
