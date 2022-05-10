04a05369fc07bcbc9d54129fde143f98f83efa03
Roman Nasyrov <romnasi@gmail.com>
2022-05-11 00:25:28 +0300

    feat: add throttle for header onScroll

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const/routes';
 import UserNav from '../userNav/UserNav';
+import { throttle } from 'throttle-typescript';
 
 const authorizeStatus = true; // temporary
 
@@ -9,16 +10,19 @@ export default function Header() {
   const [isSticky, setIsSticky] = useState(false);
 
   useEffect(() => {
-    window.addEventListener('scroll', getStickyStatus);
+    window.addEventListener('scroll', getStickyThrottled);
     return () => {
-      window.removeEventListener('scroll', getStickyStatus);
+      window.removeEventListener('scroll', getStickyThrottled);
     };
   });
 
   const getStickyStatus = () => {
     const scrollTop = window.scrollY;
     scrollTop >= 50 ? setIsSticky(true) : setIsSticky(false);
+    console.log('scrollTop:', scrollTop);
   };
+
+  const getStickyThrottled = throttle(getStickyStatus, 100);
 
   const headerClass = isSticky ? 'header header--sticky' : 'header';
 
