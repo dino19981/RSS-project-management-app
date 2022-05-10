d746ea8ef25e213d505c20218ae2c4ac637ca2aa
excluz1v <bmd4work@gmail.com>
2022-05-10 14:45:49 +0600

    feat: add fake tasks for visibility

--- src/components/Columns/Column.tsx
+++ src/components/Columns/Column.tsx
@@ -1,23 +1,72 @@
 import React, { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { TColumn } from '../../models/column';
 import { TTask } from '../../models/task';
-import Task from '../Task/Task';
 import TaskPreview from '../Task/TaskPreview';
+
+const fakeTasks = [
+  {
+    id: '6e3abe9c-ceb1-40fa-9a04-eb2b2184daf9',
+    title: 'Task: pet the cat',
+    order: 1,
+    done: false,
+    description: 'Domestic cat needs to be stroked gently',
+    userId: 'b2d92061-7d23-4641-af52-dd39f95b99f8',
+    files: [
+      {
+        filename: 'foto.jpg',
+        fileSize: 6105000,
+      },
+    ],
+  },
+  {
+    id: '6e3abe9c-ceb1-40fa-9a04-234234234',
+    title: 'Task:qwqereertert',
+    order: 2,
+    done: false,
+    description: 'D123123123ly',
+    userId: 'b2d92061-7d23-4641-af52-dd39f95b99f8',
+    files: [],
+  },
+];
 
 function Column() {
   let tasks: undefined | TTask[];
-  let column: undefined | TColumn;
+  //fake data
+  const column: undefined | TColumn = {
+    id: '7b0b41b3-c01e-4139-998f-3ff25d20dc4f',
+    title: 'Backlog',
+    order: 1,
+    tasks: [
+      {
+        id: '6e3abe9c-ceb1-40fa-9a04-eb2b2184daf9',
+        title: 'Task: pet the cat',
+        order: 1,
+        done: false,
+        description: 'Domestic cat needs to be stroked gently',
+        userId: 'b2d92061-7d23-4641-af52-dd39f95b99f8',
+        files: [
+          {
+            filename: 'foto.jpg',
+            fileSize: 6105000,
+          },
+        ],
+      },
+    ],
+  };
   useEffect(() => {
     //TODO Загрузка  колонки /boards/:boardId/columns/:columnId
   }, []);
 
   return (
-    <div className="column">
-      {column && <div className="column_title">{column.title}</div>}
-      {tasks &&
-        tasks.map((task) => {
-          return <TaskPreview key={task.id} />;
-        })}
+    <div className="columns_wrapper">
+      <div className="column">
+        {column && <div className="column_title">{column.title}</div>}
+        {fakeTasks &&
+          fakeTasks.map((task) => {
+            return <TaskPreview key={task.id} {...task} />;
+          })}
+      </div>
     </div>
   );
 }
