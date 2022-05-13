5f8e665997127a1e011159dbf5b6aef64c96a6b8
Roman Nasyrov <romnasi@gmail.com>
2022-05-13 04:05:09 +0300

    feat: init store

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/sass/main.scss';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './store/root-reducer';
+import { instanceAxios } from './HTTP/configuration';
+import { Provider } from 'react-redux';
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: instanceAxios,
+      },
+    }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
