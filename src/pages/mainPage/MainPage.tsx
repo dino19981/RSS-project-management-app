6551916e59d5b9b0b149739c1bb5dd7d4140c913
Roman Nasyrov <romnasi@gmail.com>
2022-05-21 10:47:19 +0300

    feat: change team card markup

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -88,19 +88,36 @@ export default function MainPage() {
       <section className="team">
         <h2 className="team__title">Наша команда</h2>
         <ul className="team__list">
-          {teamMembers.map(({ id, name, role }) => (
+          {teamMembers.map(({ id, name, role, ghLink }) => (
             <li className="team__item" key={id}>
-              <article className="team-member">
-                <header className="team-member__base-info">
-                  <div className="team-member__text-wrapper">
-                    <h3 className="team-member__title">{name}</h3>
-                    <span className="team-member__role">{role}</span>
-                  </div>
-                  <div className="team-member__image-wrapper">
-                    <img src="" alt={name} width="60" height="60" />
-                  </div>
-                </header>
-                <p className="team-member__desc">Занимался тем-то и тем-то, делал пятое-десятое</p>
+              <article className="team__card">
+                <h3 className="team__name">{name}</h3>
+                <span className="team__role">{role}</span>
+
+                <div>
+                  <img
+                    className="team__avatar"
+                    width="200"
+                    height="200"
+                    src="https://i.pravatar.cc/300"
+                    alt={`${name} - член команды разработки`}
+                  />
+                </div>
+
+                <p className="team__work">Сделал то то и то, работал над пятым десятым</p>
+
+                <a className="team__member-link" href={ghLink} title="Посмотреть Github аккаунт">
+                  <span className="visually-hidden">Ссылка на гитхаб</span>
+                  <svg
+                    className="team__gh-icon"
+                    width="40"
+                    height="40"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1024 1024"
+                  >
+                    <use xlinkHref="#github-icon" />
+                  </svg>
+                </a>
               </article>
             </li>
           ))}
