5c1e3bde150d625eac14d9d8904d6b6835c8a180
Roman Nasyrov <romnasi@gmail.com>
2022-05-22 16:34:03 +0300

    fix: add Team component

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,6 +1,7 @@
 import AboutCourse from '../../components/aboutCourse/AboutCourse';
 import Features from '../../components/features/Features';
 import PromoScreen from '../../components/promoScreen/PromoScreen';
+import Team from '../../components/team/Team';
 
 export default function MainPage() {
   return (
@@ -8,6 +9,7 @@ export default function MainPage() {
       <PromoScreen />
       <Features />
       <AboutCourse />
+      <Team />
     </>
   );
 }
