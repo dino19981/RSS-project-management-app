cd97e21f5bef78772caf409d3cca27d75cdc1091
Roman Nasyrov <romnasi@gmail.com>
2022-05-22 09:53:24 +0300

    feat: add adoptive styles for tablet, desktop

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -6,24 +6,22 @@ export default function MainPage() {
   return (
     <>
       <section className="promo-screen">
-        <div className="promo-screen__text-wrapper">
-          <h1 className="promo-screen__title">Мастерски управляй проектами!</h1>
-          <div className="promo-screen__image-wrapper">
-            <img
-              className="promo-screen__image"
-              width="667"
-              height="644"
-              src="assets/images/image-hero.png"
-              alt="Иллюстрация лотка с документами"
-            />
-          </div>
-          <p className="promo-screen__desc">
-            Бесплатное приложение для командной работы над проектами
-          </p>
-          <Link className="promo-screen__btn" to={AppRoute.REGISTRATION}>
-            Начать
-          </Link>
+        <h1 className="promo-screen__title">Мастерски управляй проектами!</h1>
+        <div className="promo-screen__image-wrapper">
+          <img
+            className="promo-screen__image"
+            width="667"
+            height="644"
+            src="assets/images/image-hero.png"
+            alt="Иллюстрация лотка с документами"
+          />
         </div>
+        <p className="promo-screen__desc">
+          Бесплатное приложение для командной работы над проектами
+        </p>
+        <Link className="promo-screen__btn" to={AppRoute.REGISTRATION}>
+          Начать
+        </Link>
       </section>
 
       <section className="features">
--- src/views/footer/Footer.tsx
+++ src/views/footer/Footer.tsx
@@ -10,31 +10,23 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container container">
-        <div className="footer__wrapper">
-          <Logo />
+        <ul className="footer__gh-list">
+          {teamMembers.map(({ id, name }) => (
+            <li key={id} className="footer__gh-item">
+              <a className="footer__author-link" href="https://github.com/Romnasi">
+                <svg className="footer__gh-icon" width="20" height="20">
+                  <use xlinkHref="#github-icon" />
+                </svg>
+                <span className="footer__member-name">{name}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
 
-          <ul className="footer__gh-list">
-            {teamMembers.map(({ id, name }) => (
-              <li key={id} className="footer__gh-item">
-                <a className="footer__author-link" href="https://github.com/Romnasi">
-                  <svg className="footer__gh-icon" width="20" height="20">
-                    <use xlinkHref="#github-icon" />
-                  </svg>
-                  <span className="footer__member-name">{name}</span>
-                </a>
-              </li>
-            ))}
-          </ul>
-
-          <UserNav authorizeStatus={authorizeStatus} />
-        </div>
-
-        <div className="footer__logo-wrapper">
-          <a className="footer__link footer__link--rss" href="https://rs.school/js/">
-            <img src="assets/images/rss-logo.svg" width="116" height="43" alt="RS School logo" />
-          </a>
-          <span className="footer__year">2022</span>
-        </div>
+        <span className="footer__year">2022</span>
+        <a className="footer__link footer__link--rss" href="https://rs.school/js/">
+          <img src="assets/images/rss-logo.svg" width="116" height="43" alt="RS School logo" />
+        </a>
       </div>
     </footer>
   );
