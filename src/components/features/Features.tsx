d8c0905bf41d9816e3d474993b916594c5249fcc
Roman Nasyrov <romnasi@gmail.com>
2022-05-29 01:39:22 +0300

    feat: add features content

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import { featuresData } from './featuresData';
 
 export default function Features() {
   const { t } = useTranslation();
@@ -6,59 +7,23 @@ export default function Features() {
     <section className="features">
       <h2 className="features__title">{t('welcome.advantages')}</h2>
       <ul className="features__list">
-        <li className="features__item">
-          <article className="feature">
-            <div className="feature__image-wrapper">
-              <img
-                className="feature__image feature__image--note"
-                src="assets/images/note.png"
-                alt="Планшет с галочкой"
-              />
-            </div>
-            <div className="feature__text-wrapper">
-              <h3 className="feature__title">Преимущество 1</h3>
-              <p className="feature__desc">
-                Задачи больше не теряются - все записано и отмечено, важные файлы прикреплены
-              </p>
-            </div>
-          </article>
-        </li>
-
-        <li className="features__item">
-          <article className="feature">
-            <div className="feature__image-wrapper">
-              <img
-                className="feature__image feature__image--money"
-                src="assets/images/money.png"
-                alt="Деньги"
-              />
-            </div>
-            <div className="feature__text-wrapper">
-              <h3 className="feature__title">Преимущество 1</h3>
-              <p className="feature__desc">
-                Задачи больше не теряются - все записано и отмечено, важные файлы прикреплены
-              </p>
-            </div>
-          </article>
-        </li>
-
-        <li className="features__item">
-          <article className="feature">
-            <div className="feature__image-wrapper">
-              <img
-                className="feature__image feature__image--calendar"
-                src="assets/images/calendar.png"
-                alt="Календарь"
-              />
-            </div>
-            <div className="feature__text-wrapper">
-              <h3 className="feature__title">Преимущество 1</h3>
-              <p className="feature__desc">
-                Задачи больше не теряются - все записано и отмечено, важные файлы прикреплены
-              </p>
-            </div>
-          </article>
-        </li>
+        {featuresData.map(({ featureName, altText, title, description }) => (
+          <li className="features__item" key={featureName}>
+            <article className="feature">
+              <div className="feature__image-wrapper">
+                <img
+                  className={`feature__image feature__image--${featureName}`}
+                  src={`assets/images/${featureName}.png`}
+                  alt={altText}
+                />
+              </div>
+              <div className="feature__text-wrapper">
+                <h3 className="feature__title">{title}</h3>
+                <p className="feature__desc">{description}</p>
+              </div>
+            </article>
+          </li>
+        ))}
       </ul>
     </section>
   );
---
+++ src/components/features/featuresData.ts
@@ -0,0 +1,23 @@
+export const featuresData = [
+  {
+    title: 'Работай эффективней',
+    description: 'Задачи больше не теряются - все записано и отмечено, важные файлы прикреплены',
+    imgSrc: 'assets/images/note.png',
+    featureName: 'note',
+    altText: 'Блокнот с галочкой',
+  },
+  {
+    title: 'Используй бесплатно',
+    description: 'Приложение доступно абсолютно бесплатно. Без смс, после регистрации.',
+    imgSrc: 'assets/images/money.png',
+    featureName: 'money',
+    altText: 'Деньги',
+  },
+  {
+    title: 'Экономь время',
+    description: 'Задачи больше не теряются - все записано и отмечено, важные файлы прикреплены',
+    imgSrc: 'assets/images/calendar.png',
+    featureName: 'calendar',
+    altText: 'Календарь',
+  },
+];
